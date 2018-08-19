a95a6c0a2ec284884759d49cc49fb2a57970f698
Aleksei Arro <polsck@gmail.com>
2018-08-20 00:58:26 +0300

    Gulp settings changed:
    gulp-sass added

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-var gulp = require('gulp');
+var
+  gulp = require('gulp'),
+  less = require('gulp-less'),
+  sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('source/sass/style.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('source/css'))
+});
