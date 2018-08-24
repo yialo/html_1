add4bdaa3f4506ddaaf96a42637b78a5b916138d
Aleksei Arro <polsck@gmail.com>
2018-08-25 02:51:38 +0300

    gulpfile.js modified, BrowserSync is adjusted well

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-var server = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
 
 gulp.task('style', function() {
   return gulp.src('source/sass/style.scss')
@@ -15,4 +15,12 @@ gulp.task('style', function() {
       autoprefixer
     ]))
     .pipe(gulp.dest('source/css'));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "source"
+    }
+  });
 });
