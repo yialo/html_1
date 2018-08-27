cd650a6becf61494685ea9ae9660a4756bf0ec10
Aleksei Arro <polsck@gmail.com>
2018-08-27 17:00:24 +0300

    Index page ready all but modal

--- source/js/script.js
+++ source/js/script.js
@@ -10,6 +10,9 @@ button_1.addEventListener('click', function(evt) {
   slide_1.classList.add('advantages__item--current');
   slide_2.classList.remove('advantages__item--current');
   slide_3.classList.remove('advantages__item--current');
+  button_1.classList.add('controls__button--active');
+  button_2.classList.remove('controls__button--active');
+  button_3.classList.remove('controls__button--active');
 });
 
 button_2.addEventListener('click', function(evt) {
@@ -17,6 +20,9 @@ button_2.addEventListener('click', function(evt) {
   slide_1.classList.remove('advantages__item--current');
   slide_2.classList.add('advantages__item--current');
   slide_3.classList.remove('advantages__item--current');
+  button_1.classList.remove('controls__button--active');
+  button_2.classList.add('controls__button--active');
+  button_3.classList.remove('controls__button--active');
 });
 
 button_3.addEventListener('click', function(evt) {
@@ -24,4 +30,7 @@ button_3.addEventListener('click', function(evt) {
   slide_1.classList.remove('advantages__item--current');
   slide_2.classList.remove('advantages__item--current');
   slide_3.classList.add('advantages__item--current');
+  button_1.classList.remove('controls__button--active');
+  button_2.classList.remove('controls__button--active');
+  button_3.classList.add('controls__button--active');
 });
