b84d3b9bf453db5fed0650470aae4fbb617aadfc
Aleksei Arro <polsck@gmail.com>
2018-08-30 15:33:11 +0300

    Scripts changed: js files splitted by separate functions

--- source/js/slider.js
+++ source/js/slider.js
@@ -5,11 +5,6 @@ var slide_1 = document.querySelector('.advantages__item--expensive');
 var slide_2 = document.querySelector('.advantages__item--math');
 var slide_3 = document.querySelector('.advantages__item--night');
 
-var feedbackLink = document.querySelector('.contacts__link');
-var modalForm = document.querySelector('.feedback');
-var fullnameField = document.querySelector('.feedback__field--fullname');
-var modalClose = document.querySelector('.feedback__close');
-
 button_1.addEventListener('click', function(evt) {
   evt.preventDefault();
   slide_1.classList.add('advantages__item--current');
@@ -38,15 +33,4 @@ button_3.addEventListener('click', function(evt) {
   button_1.classList.remove('controls__button--active');
   button_2.classList.remove('controls__button--active');
   button_3.classList.add('controls__button--active');
-});
-
-feedbackLink.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  modalForm.classList.add('show');
-  fullnameField.focus();
-});
-
-modalClose.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  modalForm.classList.remove('show');
 });
