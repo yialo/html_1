b672fadd14f7007d9f00c9faab7ddc134c3a5fff
Aleksei Arro <polsck@gmail.com>
2018-08-30 18:22:40 +0300

    Modal popup logics and animations added

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,12 +1,20 @@
 var feedbackLink = document.querySelector('.contacts__link');
 var modalForm = document.querySelector('.feedback');
 var fullnameField = document.querySelector('.feedback__field--fullname');
+var emailField = document.querySelector('.feedback__field--email');
+var messageField = document.querySelector('.feedback__field--textarea');
 var modalClose = document.querySelector('.feedback__close');
 
 feedbackLink.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalForm.classList.add('show');
   fullnameField.focus();
+});
+
+modalClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalForm.classList.remove('show');
+  modalForm.classList.remove('error');
 });
 
 feedbackLink.addEventListener('keydown', function(evt) {
@@ -16,7 +24,20 @@ feedbackLink.addEventListener('keydown', function(evt) {
   }
 });
 
-modalClose.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  modalForm.classList.remove('show');
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+    if (modalForm.classList.contains('show')) {
+      evt.preventDefault();
+      modalForm.classList.remove('show');
+    }
+  }
+});
+
+modalForm.addEventListener('submit', function(evt) {
+  if (!fullnameField.value || !emailField.value || !messageField.value) {
+    evt.preventDefault();
+    modalForm.classList.remove('error');
+    modalForm.offsetWidth = modalForm.offsetWidth;
+    modalForm.classList.add('error');
+  }
 });
