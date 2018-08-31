f80fb423f0310d4becd693c17950550f6d80866f
Aleksei Arro <polsck@gmail.com>
2018-08-31 15:21:11 +0300

    footer-map fallback added

--- source/js/footer.js
+++ source/js/footer.js
@@ -46,7 +46,9 @@ function initMap() {
   var target = {lat: 59.938760, lng: 30.323036};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
-    center: target
+    center: target,
+    mapTypeControl: false,
+    streetViewControl: false
   });
   var marker = new google.maps.Marker({position: target, map: map});
 }
